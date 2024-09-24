652f10a51cb31b321998e1215559e799a145f997
ganezapratama <133939614+ganezapratama@users.noreply.github.com>
2024-09-24 23:36:52 +0700

    Add files via upload

---
+++ percobaan 1.1.cpp
@@ -0,0 +1,15 @@
+/* Nama program : percobaan 1.1.cpp
+Nama : Ganeza pratama
+NIM : 24343116
+Tangal buat : 23 September
+Deskripsi : Pencetakan Hello Unp 2 Kali
+_______________________________________________*/
+#include <iostream>
+using namespace std;
+int main (){
+	cout << "Hello, Unp." <<endl;
+	cout << "Hello, " << "Unp ";
+	cout << "Padang dan sumbar" <<endl;
+	int n = 66;
+	cout<<n<<endl;
+}
